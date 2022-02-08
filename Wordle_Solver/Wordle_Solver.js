504904d08adb67986a471be9399aa572bfa17baa
Bastian (Bart) Stassen, P.Eng <BartmanEH@users.noreply.github.com>
2022-02-07 19:16:19 -0500

    revert

--- Wordle_Solver/Wordle_Solver.js
+++ Wordle_Solver/Wordle_Solver.js
@@ -78,9 +78,9 @@ function inputKeyup(evt) {
   $('[tabindex=' + tabindex + ']').focus();
   */
   //if ($(evt.target).next('input') != null) {                               //and next focus field is available
-  //$(evt.target).next('input').focus();                                   //focus next field
+  $(evt.target).next('input').focus();                                   //focus next field
   //const nextTab = $(evt.target).attr('tabindex') + 1;
-  $('input[tabindex=' + nextTab + ']').focus();
+  //$('input[tabindex=' + nextTab + ']').focus();
   //$(evt.target).parent().find('input[type="text"]').focus();
   //focusNextElement();
   //$('#guess_2_1').focus();
