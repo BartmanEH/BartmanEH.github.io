d4c8946e57b6c7a79a83d5ffe9d5c9e0b54c5dbe
BartmanEH <bartman.eh@gmail.com>
2022-10-13 09:06:56 -0400

    added fireworks options comment

--- Wordle_Solver/Wordle_Solver.js
+++ Wordle_Solver/Wordle_Solver.js
@@ -515,6 +515,55 @@ function celebrate(guessPosition, message) {                        //Easter Egg
   }//if
   fireworks = new Fireworks(container, { /* options */ });          //eslint-disable-line
   fireworks.setOptions({ traceSpeed: 2 });                          //fireworks options
+  /*{
+    autoresize: true,
+    opacity: 0.5,
+    acceleration: 1.05,
+    friction: 0.97,
+    gravity: 1.5,
+    particles: 50,
+    trace: 3,
+    traceSpeed: 10,
+    explosion: 5,
+    intensity: 30,
+    flickering: 50,
+    lineStyle: 'round',
+    hue: {
+      min: 0,
+      max: 360
+    },
+    delay: {
+      min: 15,
+      max: 30
+    },
+    rocketsPoint: {
+      min: 50,
+      max: 50
+    },
+    lineWidth: {
+      explosion: {
+        min: 1,
+        max: 3
+      },
+      trace: {
+        min: 1,
+        max: 2
+      }
+    },
+    brightness: {
+      min: 50,
+      max: 80
+    },
+    decay: {
+      min: 0.015,
+      max: 0.03
+    },
+    mouse: {
+      click: false,
+      move: false,
+      max: 1
+    }
+  }*/
   fireworks.start();                                                //launch fireworks effect
 }//celebrate()
 //#endregion helper functions
